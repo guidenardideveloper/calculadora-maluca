c81353a736b4dbb3b8f5cd75c34782e9f8fb888f
Guilherme Denardi <gdenardi@outlook.com>
2024-05-03 02:21:50 -0300

    Testing third calculator logic

--- src/calculators/calculator_3.py
+++ src/calculators/calculator_3.py
@@ -21,7 +21,7 @@ class Calculator3:
         multiplication = self.__calculate_multiplication(input_data)
         self.__verify_results(variance, multiplication)
 
-        formated_response = self.__format_response(multiplication)
+        formated_response = self.__format_response(variance)
         return formated_response
 
 
