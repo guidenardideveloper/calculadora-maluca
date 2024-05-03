4bc519c51d91bdc1f517a70156e36aeff9455760
Guilherme Denardi <gdenardi@outlook.com>
2024-05-03 00:57:35 -0300

    applying interfaces to the project

--- src/calculators/calculator_2_test.py
+++ src/calculators/calculator_2_test.py
@@ -1,5 +1,6 @@
 from typing import Dict
 from .calculator_2 import Calculator2
+from src.drivers.numpy_handler import NumpyHandler
 
 class MockRequest:
     def __init__(self, body: Dict) -> None:
@@ -8,10 +9,9 @@ class MockRequest:
 def test_calculate():
     mock_request = MockRequest({"numbers": [2.12, 1.34, 4.67]})
 
-    calculator_2 = Calculator2()
+    driver = NumpyHandler()
+    calculator_2 = Calculator2(driver)
     formated_response = calculator_2.calculate(mock_request)
-    print()
-    print(formated_response)
 
     assert isinstance (formated_response, dict)
     assert formated_response == {'data': {'Calculator': 2, 'result': 0.08}}
